09c89daca35d82507e8e4dd0a2d3fdaa622a7429
ryicoh <ryicoh@gmail.com>
2022-08-22 18:45:25 +0900

    7章クリア〜！

--- src/Entity/Money.php
+++ src/Entity/Money.php
@@ -1,14 +1,16 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Ikucmknk\TddMoney\Entity;
 
 class Money
 {
-    protected int $amount;
+  protected int $amount;
 
-    public function equals(Money $Money): bool
-    {
-        return $Money->amount === $this->amount;
-    }
-
+  public function equals(Money $Money): bool
+  {
+    return $Money->amount === $this->amount
+      && get_class($this) === get_class($Money);
+  }
 }
--- test/MoneyTest.php
+++ test/MoneyTest.php
@@ -19,11 +19,12 @@ class MoneyTest extends TestCase
         $this->assertFalse((new Dollar(5))->equals(new Dollar(6)));
         $this->assertTrue((new Franc(5))->equals(new Franc(5)));
         $this->assertFalse((new Franc(5))->equals(new Franc(6)));
+        $this->assertFalse((new Franc(5))->equals(new Dollar(5)));
     }
 
     public function testFrancMultiplication()
     {
-            $Five = new Franc(5);
+        $Five = new Franc(5);
         $this->assertEquals(new Franc(10), $Five->times(2));
         $this->assertEquals(new Franc(15), $Five->times(3));
     }
